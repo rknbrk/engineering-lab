2cb52e3b4860ea5f57c760b0ab756e184f6cb35d
Erkan Berk <erkan.berk@awtg.co.uk>
2022-04-21 21:44:31 +0000

    Add C util header: rol32

---
+++ c/include/util/rol32.h
@@ -0,0 +1,7 @@
+#ifndef UTIL_ROL32_H
+#define UTIL_ROL32_H
+#include <stdint.h>
+static inline uint32_t util_rol32(uint32_t value, unsigned int shift) {
+  shift &= 31u; return (value << shift) | (value >> (32u - shift));
+}
+#endif
